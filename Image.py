8d55df2bff33bb179df5697c3c4d109e653e0c88
Alex <a@ak.rs>
2011-12-24 15:33:36 +0000

    code refactoring - now using string formats instead of concats

--- Image.py
+++ Image.py
@@ -1,29 +1,23 @@
 
 class Image:
 
-	url_istock = "http://www.istockphoto.com"
+	url_istock = 'http://www.istockphoto.com'
 
 	def __init__(self, width=None, height=None, caption=None, id=None):
+		self.id = id
 		self.width = width
 		self.height = height 
 		self.caption = caption
-		self.id = id
 
 
 	def get_dims(self):
 		return [self.width, self.height]
 
 
-	def get_thumb_src(self, thumb_size=1):
-		url_thumb  = '/file_thumbview/'
-		return self.url_istock + url_thumb + self.id + '/' + str(thumb_size) + '/' 
+	def get_thumb_src(self, thumb_size='1'):
+		return '{0}/file_thumbview/{1}/{2}/'.format(self.url_istock, self.id, thumb_size)
 
 
 	def get_ubb_string(self):
-		delimiter = ' '
-		url_detail = '/file_closeup.php?id='
-		ubb	  = "[url=" + self.url_istock + url_detail + self.id + "]"
-		ubb  +=	"[img]" + self.get_thumb_src() + "[/img]"
-		ubb  += "[/url]"
-		return ubb
+		return '[url={0}{1}{2}][img]{3}[/img][/url] '.format(self.url_istock, '/file_closeup.php?id=', self.id, self.get_thumb_src())
 
